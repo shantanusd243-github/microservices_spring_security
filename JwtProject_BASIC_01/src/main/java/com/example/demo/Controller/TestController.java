c26d6c61e9a61030185e8591767bb554db7de5e0
mudassar110 <mudassarnawab10@gmail.com>
2024-06-23 23:21:03 +0530

    BY Mudassar: JWT_BASIC_01

---
+++ JwtProject_BASIC_01/src/main/java/com/example/demo/Controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/")
+public class TestController {
+
+	@GetMapping("/home")
+	public String welcome()
+	{
+		return "Welcome to home";
+		
+	}
+}
