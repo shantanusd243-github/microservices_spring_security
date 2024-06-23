c8ba6032d3304de3dddd59fe6a8623f3b4cce701
mudassar110 <mudassarnawab10@gmail.com>
2024-06-23 23:26:48 +0530

    BY Mudassar: Basic_02-1_jWT

---
+++ JwtProject_BASIC_02-1/src/main/java/com/example/demo/JwtProjectBasic021Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtProjectBasic021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtProjectBasic021Application.class, args);
+	}
+
+}
---
+++ JwtProject_BASIC_02-1/src/test/java/com/example/demo/JwtProjectBasic021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtProjectBasic021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
